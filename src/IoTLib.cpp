a80edffd6dd70a94f3ef6d5d6cb4b1e7c4fd488c
Chetan Gadgil <c.a.gadgil@gmail.com>
2017-06-10 09:57:18 -0700

    initial version

---
+++ src/IoTLib.cpp
@@ -0,0 +1,17 @@
+#include <IoTLib.hpp>
+
+void IoTLib::setup()
+{
+ Serial.begin(9600);
+ WiFiManager wifiManager;
+ String apName = "provision-me-" + String(ESP.getChipId());
+ Serial.println(apName);
+ // id/name, placeholder/prompt, default, length
+ WiFiManagerParameter fieldGateway("gateway", "field gateway", gatewayAddress, 40);
+ wifiManager.addParameter(&fieldGateway);
+ //wifiManager.autoConnect(apName.c_str());
+ // Comment line below and uncomment the autoconnect above to remember last settings
+ wifiManager.startConfigPortal(apName.c_str());
+ strncpy(gatewayAddress, fieldGateway.getValue(), 40);
+ //timeClient.begin();
+}
--- src/IoTLib.hpp
+++ src/IoTLib.hpp
@@ -15,22 +15,9 @@
 
 class IoTLib {
   WiFiUDP ntpUDP;
-  NTPClient timeClient(ntpUDP, NTP_ADDRESS, NTP_OFFSET, NTP_INTERVAL);
+  //NTPClient timeClient(ntpUDP, NTP_ADDRESS, NTP_OFFSET, NTP_INTERVAL);
   char gatewayAddress[40];
 
 public:
-  void setup() {
-    Serial.begin(9600);
-    WiFiManager wifiManager;
-    String apName = "provision-me-" + String(ESP.getChipId());
-    Serial.println(apName);
-    // id/name, placeholder/prompt, default, length
-    WiFiManagerParameter fieldGateway("gateway", "field gateway", gatewayAddress, 40);
-    wifiManager.addParameter(&fieldGateway);
-    //wifiManager.autoConnect(apName.c_str());
-    // Comment line below and uncomment the autoconnect above to remember last settings
-    wifiManager.startConfigPortal(apName.c_str());
-    strncpy(gatewayAddress, fieldGateway.getValue(), 40);
-    timeClient.begin();
-  }
-}
+  void setup();
+};
